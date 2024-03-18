9f85e96735d1d2cc8c597debbaec98d62961df6d
Torrel Moseley <trrl.mo@hotmail.com>
2024-03-18 16:27:51 -0400

    add a sample book that has been read

--- lib/db/insert_sample_data_into_initial_tables.sql
+++ lib/db/insert_sample_data_into_initial_tables.sql
@@ -60,12 +60,49 @@ INSERT INTO tags (book_id, title, created_at, updated_at)
       now()
     );
 
+INSERT INTO books (
+  user_id,
+  author,
+  page_count,
+  theme,
+  source,
+  title,
+  description,
+  created_at,
+  updated_at
+)
+    VALUES(
+      1,
+      'Atul Gawande',
+      193,
+      'Business',
+      'Hilary at Alfred',
+      'The Checklist Manifesto',
+      'How to get things right',
+      now(),
+      now()
+    );
+
+INSERT INTO categories (book_id, name, created_at, updated_at)
+     VALUES (2, 'Business', now(), now());
+
 INSERT INTO goals (user_id, title, term_date, description, created_at, updated_at)
      VALUES (
        1,
        'Yearly reading goal',
        '2024-12-31 23:59:59',
        'Read 17 books this year',
+       now(),
+       now()
+     );
+
+INSERT INTO goals (user_id, title, term_date, description, value, created_at, updated_at)
+     VALUES (
+       1,
+       'Montly reading goal',
+       '2024-2-29 23:59:59',
+       'Read a book by the end of the month',
+       1,
        now(),
        now()
      );
@@ -83,6 +120,23 @@ INSERT INTO events (user_id, book_id, type, value, created_at, updated_at)
        1,
        'READ',
        5,
+       now(),
+       now()
+     );
+
+INSERT INTO events (user_id, book_id, type, created_at, updated_at)
+     VALUES (
+       1,
+       2,
+       'READING COMPLETE',
+       now(),
+       now()
+     );
+
+INSERT INTO goals_events (goal_id, event_id, created_at, updated_at)
+     VALUES (
+       2,
+       3,
        now(),
        now()
      );
